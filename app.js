834a7cfb3130534ef23d1a8a98f273edc5296654
valentingheorghev <valentin.gheorghev@gmail.com>
2026-02-26 20:34:53 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,63 +1,29 @@
-// 1) Inițializăm harta
+// 1. Inițializăm harta
 const map = L.map('map').setView([45.9, 24.9], 7);
 
-// 2) Grup pentru UAT (îl golim la fiecare click pe județ)
-const layerUAT = L.featureGroup().addTo(map);
+// 2. Variabilă pentru layer
+let layerJudete;
 
-// 3) Încărcăm ambele GeoJSON-uri o singură dată
-let uatData = null;
+// 3. Încărcăm GeoJSON-ul cu județe
+fetch('judete.geojson')
+  .then(r => r.json())
+  .then(data => {
 
-Promise.all([
-  fetch('./judete.geojson').then(r => r.json()),
-  fetch('./uat.geojson').then(r => r.json()),
-]).then(([judeteData, uat]) => {
-  uatData = uat;
+    layerJudete = L.geoJSON(data, {
+      style: {
+        color: '#444',
+        weight: 1,
+        fillColor: '#9ecae1',
+        fillOpacity: 0.7
+      },
+      onEachFeature: (feature, layer) => {
 
-  // 4) Desenăm județele
-  L.geoJSON(judeteData, {
-    style: {
-      color: '#444',
-      weight: 1,
-      fillColor: '#9ecae1',
-      fillOpacity: 0.7
-    },
-    onEachFeature: (feature, layer) => {
-      layer.on('click', () => {
-        const judet = feature.properties.Judet;
-        showUATForJudet(judet);
-      });
-    }
-  }).addTo(map);
-});
+        // La click pe județ (DOAR afișăm numele)
+        layer.on('click', () => {
+          alert(feature.properties.Judet);
+        });
 
-function showUATForJudet(judet) {
-  // 5) Ștergem UAT-urile vechi
-  layerUAT.clearLayers();
+      }
+    }).addTo(map);
 
-  // 6) Adăugăm doar UAT-urile din județul selectat (filter)
-  const uatLayer = L.geoJSON(uatData, {
-    filter: (f) => f.properties.Judet === judet,
-    style: {
-      color: '#d94801',
-      weight: 1,
-      fillColor: '#fd8d3c',
-      fillOpacity: 0.6
-    },
-    onEachFeature: (f, l) => {
-      l.on('click', () => {
-        const url = f.properties.URL;
-        if (url) window.open(url, '_blank');
-        else alert('Lipsește proprietatea URL la acest UAT.');
-      });
-    }
   });
-
-  layerUAT.addLayer(uatLayer);
-
-  // 7) Zoom pe UAT-urile din județ (dacă există)
-  if (layerUAT.getLayers().length > 0) {
-    map.fitBounds(layerUAT.getBounds(), { padding: [10, 10] });
-  } else {
-    alert('Nu am găsit UAT-uri pentru județul selectat.');
-  }
-}
